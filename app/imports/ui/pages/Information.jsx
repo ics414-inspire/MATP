eb6622d230a3160595a57c7329f5e31b7e7bbcd1
ChronoTrigger64 <sydneey@hawaii.edu>
2024-09-26 01:29:12 -1000

    playing around with info page to get button pop up to work
    this will be moved to the audited balance page later on.

--- app/imports/ui/pages/Information.jsx
+++ app/imports/ui/pages/Information.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Col, Container, Image, Row } from 'react-bootstrap';
+
+import { Col, Container, Image, Row, Tooltip} from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
 import { PAGE_IDS } from '../utilities/PageIDs';
 
-const Information = () => (
+const Information = () => {
   <Container id={PAGE_IDS.LANDING} className="py-3">
     <Row className="align-middle text-center">
       <Col xs={4}>
@@ -15,6 +18,21 @@ const Information = () => (
 
     </Row>
   </Container>
-);
 
+return (
+  <div style={{ display: 'block', width: 700, padding: 30 }}>
+    <h4>React-Bootstrap Tooltip Component</h4>
+    <OverlayTrigger
+      delay={{ hide: 450, show: 300 }}
+      overlay={(props) => (
+        <Tooltip {...props}>
+          Hii, I am a simple tooltip information!!!
+        </Tooltip>
+      )}
+      placement="bottom"
+    ><Button variant="warning">Tooltip Button</Button>
+    </OverlayTrigger>,
+  </div>
+);
+};
 export default Information;
