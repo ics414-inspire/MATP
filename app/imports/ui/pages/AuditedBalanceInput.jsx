06f1f7c832bbb513b3caee6210d6bfe7f55b68b4
ChronoTrigger64 <sydneey@hawaii.edu>
2024-10-01 21:38:09 -1000

    added appropriated definitions to audited balance input page

--- app/imports/ui/pages/AuditedBalanceInput.jsx
+++ app/imports/ui/pages/AuditedBalanceInput.jsx
@@ -6,87 +6,87 @@ import { PAGE_IDS } from '../utilities/PageIDs';
 
 const pettyCashTt = props => (
   // eslint-disable-next-line react/jsx-props-no-spreading
-  <Tooltip {...props}>Tooltip for the petty cash button</Tooltip>
+  <Tooltip {...props}>Refers to a small amount of cash set aside for small fees.</Tooltip>
 );
 
 const cashTt = props => (
   // eslint-disable-next-line react/jsx-props-no-spreading
-  <Tooltip {...props}>Tooltip for the cash button</Tooltip>
+  <Tooltip {...props}>Refers to any regular cash for any fees. </Tooltip>
 );
 
 const cashInBanksTt = props => (
   // eslint-disable-next-line react/jsx-props-no-spreading
-  <Tooltip {...props}>Tooltip for the petty cash button</Tooltip>
+  <Tooltip {...props}>Refers to a financial loan that allows a business to borrow up to a certain amount of money.</Tooltip>
 );
 
 const accountsReceiveTt = props => (
   // eslint-disable-next-line react/jsx-props-no-spreading
-  <Tooltip {...props}>Tooltip for the accounts receivable button</Tooltip>
+  <Tooltip {...props}>Outstanding payment owed to a business.</Tooltip>
 );
 
 const dueFundsTt = props => (
   // eslint-disable-next-line react/jsx-props-no-spreading
-  <Tooltip {...props}>Tooltip for the petty cash button</Tooltip>
+  <Tooltip {...props}>Refers to an asset account that keeps track of funds owed to another company at another firm.</Tooltip>
 );
 
 const interestTt = props => (
   // eslint-disable-next-line react/jsx-props-no-spreading
-  <Tooltip {...props}>Tooltip for the petty cash button</Tooltip>
+  <Tooltip {...props}>Interests receivable is interest that has yet to be collected, and dividends receivable are dividends that a company claims but have yet to be paid to the shareholders.  These are both assets.</Tooltip>
 );
 
 const prepaidTt = props => (
   // eslint-disable-next-line react/jsx-props-no-spreading
-  <Tooltip {...props}>Tooltip for the petty cash button</Tooltip>
+  <Tooltip {...props}>These are administrative expenses like legal retainers, leases, and other ongoing expenses that are paid in advance.</Tooltip>
 );
 
 const notesReceiveTt = props => (
   // eslint-disable-next-line react/jsx-props-no-spreading
-  <Tooltip {...props}>Tooltip for the petty cash button</Tooltip>
+  <Tooltip {...props}>These are written promises by the debtor that record the amount they need to pay the creditor.</Tooltip>
 );
 
 const notesReceiveAfterTt = props => (
   // eslint-disable-next-line react/jsx-props-no-spreading
-  <Tooltip {...props}>Tooltip for the petty cash button</Tooltip>
+  <Tooltip {...props}>Notes receivable expected to be paid after a year or longer (non-current asset).</Tooltip>
 );
 
 const securityTt = props => (
   // eslint-disable-next-line react/jsx-props-no-spreading
-  <Tooltip {...props}>Tooltip for the petty cash button</Tooltip>
+  <Tooltip {...props}>Sum paid to a tenant to cover any potential loss or damage.</Tooltip>
 );
 
 const cashHeldTt = props => (
   // eslint-disable-next-line react/jsx-props-no-spreading
-  <Tooltip {...props}>Tooltip for the petty cash button</Tooltip>
+  <Tooltip {...props}>On-hand liquid assets held by an investment manager for short-term obligations and liquidity needs.</Tooltip>
 );
 const mutualFundsTt = props => (
   // eslint-disable-next-line react/jsx-props-no-spreading
-  <Tooltip {...props}>Tooltip for the petty cash button</Tooltip>
+  <Tooltip {...props}>Investment vehicle where funds from multiple shareholders that trades in a variety of holdings. This program is managed by professionals.</Tooltip>
 );
 const commingledFundsTt = props => (
   // eslint-disable-next-line react/jsx-props-no-spreading
-  <Tooltip {...props}>Tooltip for the petty cash button</Tooltip>
+  <Tooltip {...props}>This is a fund accumulated from different investors into one fund courtesy of an investment manager.</Tooltip>
 );
 const hedgeFundsTt = props => (
   // eslint-disable-next-line react/jsx-props-no-spreading
-  <Tooltip {...props}>Tooltip for the petty cash button</Tooltip>
+  <Tooltip {...props}>This is a limited partnership where money from multiple investors using risky methods is pooled and managed by fund managers to make capital gains.</Tooltip>
 );
 const privateEquityTt = props => (
   // eslint-disable-next-line react/jsx-props-no-spreading
-  <Tooltip {...props}>Tooltip for the petty cash button</Tooltip>
+  <Tooltip {...props}>This refers to investments made into private companies not listed on public stock exchanges.</Tooltip>
 );
 const accountsPayableTt = props => (
   // eslint-disable-next-line react/jsx-props-no-spreading
-  <Tooltip {...props}>Tooltip for the petty cash button</Tooltip>
+  <Tooltip {...props}>Accruals are expenses that have not been received or paid yet. Accounts payable is a type of accrual where a company receives goods prior to paying for it.</Tooltip>
 );
 
 const dueToFundTt = props => (
   // eslint-disable-next-line react/jsx-props-no-spreading
-  <Tooltip {...props}>Tooltip for the petty cash button</Tooltip>
+  <Tooltip {...props}>This is the amount of funds due to another company. They can be short term or long term funds. This is generally counted as liability on the balance sheet.</Tooltip>
 );
 
 const dueToOtherFund = props => (
   // eslint-disable-next-line react/jsx-props-no-spreading
-  <Tooltip {...props}>Tooltip for the petty cash button</Tooltip>
+  <Tooltip {...props}>This is amount of funds owed to another fund.</Tooltip>
 );
 
 const AuditedBalanceInput = () => (
--- app/imports/ui/pages/Information.jsx
+++ app/imports/ui/pages/Information.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
-
-import { Col, Container, Image, Row, Tooltip } from 'react-bootstrap';
-import { QuestionCircle } from 'react-bootstrap-icons';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import { Col, Container, Image, Row } from 'react-bootstrap';
 import { PAGE_IDS } from '../utilities/PageIDs';
-
 
 const Information = () => (
   <Container id={PAGE_IDS.LANDING} className="py-3">
@@ -15,24 +11,6 @@ const Information = () => (
       <Col xs={8} className="d-flex flex-column justify-content-center">
         <h1>INFORMATION</h1>
         <p>Information things</p>
-        <div style={{ display: 'block', width: 700, padding: 30 }}>
-          <h4>React-Bootstrap Tooltip Component</h4>
-          <OverlayTrigger
-            delay={{ hide: 450, show: 300 }}
-            overlay={(props) => (
-              <Tooltip {...props}>
-                Hii, I am a simple tooltip information!!!
-              </Tooltip>
-            )}
-            placement="bottom">
-            <QuestionCircle />
-          </OverlayTrigger>,
-
-          <Tooltip>
-            hello
-          </Tooltip>
-
-        </div>
       </Col>
 
     </Row>
